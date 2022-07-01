e09c25d9096f1d545faadc56b62fc3745317195c
gbote <gerardorafaelbote@gmail.com>
2022-07-01 01:48:01 -0400

    Updated Projects.js, 7/1 12:47AM

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -45,7 +45,7 @@ function Projects() {
             <ProjectCard
               imgPath={editor}
               isBlog={false}
-              title="Editor.io"
+              title="Pokemon Theme Team"
               description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
               link="https://github.com/soumyajit4419/Editor.io"
             />
