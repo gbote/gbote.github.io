eaec68006fc6909f16f0a48f0f5fa0b786c16dbe
gbote <gerardorafaelbote@gmail.com>
2022-07-30 01:32:15 -0500

    Revised Projects for Simiplicity and Design, 7/30 1:30AM

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -59,7 +59,7 @@ function Projects() {
               isVideoDemo={false}
               title="Pokemon Theme Team"
               description="Pokemon Theme Team is a Heroku app built with a Django backend framework that uses the RESTful PokéAPI to generate 6 different Pokémon of similar type. The app uses vanilla HTML, CSS, and JS to construct its user interface (UI).
-              In fact, if you type a number after the link provided, you can generate a team based on a certain Pokémon's unique ID number."
+              Everytime you reload the page, you will be prompted with 6 similar Pokemon that contains their main type and their starting experience."
               ghLink="https://github.com/gbote/Pokemon-Theme-Team"
               demoLink="https://pokemonthemeteam.herokuapp.com/" 
             />
@@ -71,10 +71,10 @@ function Projects() {
               isBlog={false}
               isVideoDemo={false}
               title="Hangman Game"
-              description="My Hangman Game was the first ever React project I made using the React framework. I also experimented with using Vite as a supplement in helping build React projects, which I found helpful.
-              As per the README.md file, I created the app's required components and integrated a given JSON array of words to supply the game with. In later releases, I changed the code such that 1) words come from the Random Word API and 2) 7 incorrect guesses result in a game over. Additionally, to make the UI more pleasing to the eye, I customized my own CSS files to my liking."
+              description="My Hangman Game was the first ever (and biggest) React project I made. I also experimented with using Vite as a supplement in helping build React projects, which I found helpful.
+              Whilst I followed the README.md file in creating the app's required components and integrated a given JSON array of words to supply the game with, I decided to do more. In later releases, I changed the code such that 1) words come from the The Movie Database API and 2) 6 incorrect guesses result in a game over. If given more time, I want to make the UI more pleasing to the eye by customizing the CSS files. I appreciate Tünde Szirák for allowing me to play with the code and learn more about the complexities of React components!"
               ghLink="https://github.com/gbote/CodePlatoon/tree/main/react-hangman"
-              demoLink="https://https://hangman-kappa.vercel.app/" 
+              demoLink="https://hangman-kappa.vercel.app/" 
             />
           </Col>
 
@@ -84,7 +84,7 @@ function Projects() {
               isBlog={false}
               isVideoDemo={false}
               title="Temperature Conversion App"
-              description="The Temperature Conversion App takes in any U.S. ZIP Code and outputs the current temperature at that location to the nearest 2 decimal places. It is also both in the Celcius and Fahrenheit scales.
+              description="The Temperature Conversion App takes in any U.S. ZIP Code and outputs the current temperature at that location to the nearest 3 decimal places. It is also both in the Celcius and Fahrenheit scales.
               Similar to the Hangman Game, I implemented an API called the OpenWeather API to gather the correct information for a certain ZIP Code's temperature. Due to time constraints, I wish I included more information regarding the humidity and wind speeds of a ZIP Code. This project was also configured under the Vite/React framework."
               ghLink="https://github.com/gbote/CodePlatoon/tree/main/react-temperature-conversion"
               demoLink="https://temperature-converter-ten.vercel.app/" 
@@ -98,7 +98,7 @@ function Projects() {
               isVideoDemo={false}
               title="Palindrome Checker"
               description="Palindrome Checker is an app improved upon a design prototype built using Vite &amp; React that takes in a word via a text box, captures that typed word, and outputs whether or not that word is a palindrome.
-              Similar to the design and interface of Pokemon Theme Team, I used different colors to make the UI more appealing, including highlighting the typed word. "
+              Although it is a simpler React project compared to my Portfolio Website or my Hangman Game, I wished I included more CSS styling to make the app a bit more appealing. In other words, if given more time, I do not want this app to just look like a &quot;box.&quot;"
               ghLink="https://github.com/gbote/CodePlatoon/tree/main/react-palindrome"
               demoLink="https://palindrome-checker-omega.vercel.app/" 
             />
@@ -113,7 +113,7 @@ function Projects() {
               description="The Cars &amp; Brands CRUD App is my first ever Django project dealing with a SQL Database. Given the recommended steps in creating a database, I experimented with PostgreSQL and its numerous functions to create and manipulate databases to my liking.
               Unfortunately, the only issue I encountered when trying to publish this project is trying to establish a new database under Heroku deployment. If given more time, I would deploy the backend project with a template database so that each user's listed cars and brands are shown properly."
               ghLink="https://github.com/gbote/CodePlatoon/tree/main/django-cars-and-brands"
-              demoLink="https://youtu.be/bmmkYsPEyBU" 
+              demoLink="https://youtu.be/CmkA2x_pmA0" 
             />
           </Col>
 
@@ -139,7 +139,7 @@ function Projects() {
               description="Another Django project that I implemented was a To-Do List that required a user's e-mail and password to log in to their current To-Do items. I was able to figure out how to keep a dictionary of users' e-mails and their respective passwords.
               At this point, I was more comfortable with dealing with SQL Databases and figuring out how to deal with different relationships (i.e., one-to-one, one-to-many, many-to-many) in Django Object-Relational Mappers (ORMs). Again, similar to the Cars &amp; Brands CRUD App, I could not figure out how to make a template database for Heroku deployment; if given more time, I would love for this issue to be resolved."
               ghLink="https://github.com/gbote/CodePlatoon/tree/main/django-to-do"
-              demoLink="https://youtu.be/6jwp6m370A8" 
+              demoLink="https://youtu.be/MH2DtbolmUM" 
             />
           </Col>
 
@@ -150,7 +150,7 @@ function Projects() {
               isVideoDemo={false}
               title="Top Trending News Site"
               description="This Top Trending News Site is my second major Vite/React project in a while, where I had to integrate the Hacker News API (i.e., a Firebase containing JSON objects of HN articles).
-              Unfortunately, some of these JSON objects do not contain a URL tag, leading to some of the links being broken. Nevertheless, I figured out how to route different types of articles via a Navigation Bar and listed important information of an article, like its publication date. If given more time, I would like to implement a search bar that searches for key matches in the articles' titles."
+              Unfortunately, some of these JSON objects do not contain a URL tag, leading to some of the links being broken. Nevertheless, I figured out how to route different types of articles via a Navigation Bar and listed important information of an article, like its publication date. If given more time, I would like to implement a search bar that searches for key matches in the articles' titles. Also, I have included a search bar in hopes that the user searches for specific articles; however, it has no functionality yet."
               ghLink="https://github.com/gbote/News-Site"
               demoLink="https://news-site-coral.vercel.app/" 
             />
